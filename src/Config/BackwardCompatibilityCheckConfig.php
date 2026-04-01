737ec5ad1659731c1a194a1604dcde1ba77a2a87
Paweł Tomulik <pawel@tomulik.pl>
2026-04-01 16:07:30 +0200

    go home!

--- src/Config/BackwardCompatibilityCheckConfig.php
+++ src/Config/BackwardCompatibilityCheckConfig.php
@@ -4,44 +4,77 @@ namespace Tailors\Console\Workflows\Config;
 
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
-class BackwardCompatibilityCheckConfig implements ConfigurationInterface
+final class BackwardCompatibilityCheckConfig implements ConfigurationInterface
 {
-    public function addConfigSubtree($rootNode): void
-    {
-        $rootNode
-            ->children()
-                ->stringNode('script')
-                    ->isRequired()
-                ->end()
-                ->enumNode('command')
-                    ->values(['roave-backwards-compatibility-check:assert-backwards-compatible'])
-                ->end()
-                ->arrayNode('options', 'option')
-                    ->arrayPrototype()
-                        ->children()
-                          ->booleanNode('no_interactions')->defaultTrue()->end() // true
-                          ->booleanNode('ansi')->defaultTrue()->end() // true
-                          ->stringNode('from')->end() // null
-                          ->stringNode('to')->end() // null
-                          ->booleanNode('install_development_dependencies')->end() // null
-                          ->enumNode('format')->values(['console', 'markdown', 'github-actions'])->end() // null
-                        ->end()
-                    ->end()
-                ->end()
-                ->arrayNode('arguments', 'argument')
-                ->end()
-            ->end()
-        ;
-    }
-
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('backward_compatibility_check');
 
         $rootNode = $treeBuilder->getRootNode();
 
+        $this->defineConfigTree($rootNode);
+
         return $treeBuilder;
+    }
+
+    /**
+     * @param ArrayNodeDefinition $rootNode
+     */
+    private function defineConfigTree(ArrayNodeDefinition $rootNode): void
+    {
+        $rootNode
+            ->children()
+                ->stringNode('script')
+                    ->cannotBeEmpty()
+                    ->defaultValue($this->getDefaultScriptName())
+                ->end()
+                ->enumNode('command')
+                    ->values($this->getSupportedScriptCommands())
+                ->end()
+                ->append($this->getOptionsNode())
+                ->arrayNode('arguments', 'argument')
+                    ->stringPrototype()->end()
+                ->end()
+            ->end()
+        ;
+    }
+
+    private function getDefaultScriptName(): string
+    {
+        return 'roave-backward-compatibility-check';
+    }
+
+    /**
+     * @return list<string>
+     */
+    private function getSupportedScriptCommands(): array
+    {
+        return [
+            'roave-backwards-compatibility-check:assert-backwards-compatible'
+        ];
+    }
+
+    private function getOptionsNode(): ArrayNodeDefinition
+    {
+        $treeBuilder = new TreeBuilder('options');
+
+        $rootNode = $treeBuilder->getRootNode();
+
+        $node = $rootNode
+            ->addDefaultsIfNotSet()
+            ->children()
+              ->booleanNode('no_interactions')->defaultTrue()->end() // true
+              ->booleanNode('ansi')->defaultTrue()->end() // true
+              ->stringNode('from')->end() // null
+              ->stringNode('to')->end() // null
+              ->booleanNode('install_development_dependencies')->end() // null
+              ->enumNode('format')->values(['console', 'markdown', 'github-actions'])->end() // null
+            ->end()
+        ;
+
+        return $node;
     }
 }
 
--- tests/src/Config/BackwardCompatibilityCheckConfigTest.php
+++ tests/src/Config/BackwardCompatibilityCheckConfigTest.php
@@ -3,7 +3,134 @@
 namespace Tailors\Console\Workflows\Config;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Tailors\PHPUnit\KsortedArrayIdenticalToTrait;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 
-final class BackwardCompatibilityCheckConfigTest
+final class BackwardCompatibilityCheckConfigTest extends TestCase
 {
+    use KsortedArrayIdenticalToTrait;
+
+    /**
+     * @param array $inputs
+     * @param array $expect
+     */
+    #[DataProvider("provideWithValidConfig")]
+    public function testWithValidConfig(array $inputs, array $expect): void
+    {
+        $processor = new Processor();
+        $config = new BackwardCompatibilityCheckConfig();
+        $processedConfig = $processor->processConfiguration($config, $inputs);
+        $this->assertKsortedArrayIdenticalTo($expect, $processedConfig);
+    }
+
+    /**
+     * @param array $inputs
+     */
+    #[DataProvider("provideWithInvalidConfig")]
+    public function testWithInvalidConfig(array $inputs, string $expect): void
+    {
+        $processor = new Processor();
+        $config = new BackwardCompatibilityCheckConfig();
+
+        $this->expectException(InvalidConfigurationException::class);
+        $this->expectExceptionMessageMatches($expect);
+
+        $processedConfig = $processor->processConfiguration($config, $inputs);
+    }
+
+    /**
+     * @return iterable<array{inputs:array, expect:array}>
+     */
+    public static function provideWithValidConfig(): iterable
+    {
+        $defaults = [
+            'script' => 'roave-backward-compatibility-check',
+            'options' => [
+                'no_interactions' => true,
+                'ansi' => true
+            ],
+            'arguments' => [],
+        ];
+
+        yield [
+            'inputs' => [[]],
+            'expect' => \array_merge($defaults, [])
+        ];
+
+        yield [
+            'inputs' => [[]],
+            'expect' => $defaults,
+        ];
+
+
+        yield [
+            'inputs' => [['script' => 'foo']],
+            'expect' => \array_merge($defaults, ['script' => 'foo'])
+        ];
+
+        yield [
+            'inputs' => [['script' => 'foo'], ['script' => 'bar']],
+            'expect' => \array_merge($defaults, ['script' => 'bar'])
+        ];
+
+        yield [
+            'inputs' => [['command' => 'roave-backwards-compatibility-check:assert-backwards-compatible']],
+            'expect' => \array_merge($defaults, ['command' => 'roave-backwards-compatibility-check:assert-backwards-compatible'])
+        ];
+
+        yield [
+            'inputs' => [['options' => []]],
+            'expect' => $defaults,
+        ];
+
+        yield [
+            'inputs' => [['options' => []]],
+            'expect' => $defaults,
+        ];
+
+        yield [
+            'inputs' => [['options' => ['no_interactions' => true]]],
+            'expect' => \array_replace_recursive($defaults, ['options' => ['no_interactions' => true]]),
+        ];
+
+        yield [
+            'inputs' => [['options' => ['no_interactions' => false]]],
+            'expect' => \array_replace_recursive($defaults, ['options' => ['no_interactions' => false]]),
+        ];
+
+        yield [
+            'inputs' => [['options' => ['ansi' => true]]],
+            'expect' => \array_replace_recursive($defaults, ['options' => ['ansi' => true]]),
+        ];
+
+        yield [
+            'inputs' => [['options' => ['ansi' => false]]],
+            'expect' => \array_replace_recursive($defaults, ['options' => ['ansi' => false]]),
+        ];
+
+//              ->booleanNode('no_interactions')->defaultTrue()->end() // true
+//              ->booleanNode('ansi')->defaultTrue()->end() // true
+//              ->stringNode('from')->end() // null
+//              ->stringNode('to')->end() // null
+//              ->booleanNode('install_development_dependencies')->end() // null
+//              ->enumNode('format')->values(['console', 'markdown', 'github-actions'])->end() // null
+    }
+
+    /**
+     * @return iterable<array{inputs:array, expect:string}>
+     */
+    public static function provideWithInvalidConfig(): iterable
+    {
+        yield [
+            'inputs' => [['script' => null]],
+            'expect' => '/"backward_compatibility_check.script" cannot contain an empty value/'
+        ];
+
+        yield [
+            'inputs' => [['command' => 'foo']],
+            'expect' => '/"foo" is not allowed for path "backward_compatibility_check.command"/'
+        ];
+    }
 }
